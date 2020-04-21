5a945fe2d4dc653d5c36df90ae65d5212b7a60d8
MarcOrdan <marcordan@gmail.com>
2020-04-21 21:22:48 +0800

    Get facebox

--- src/App.js
+++ src/App.js
@@ -32,8 +32,29 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {}
     }
+  }
+
+  calculateFaceLocation = (data) => {
+   const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+   const image = document.getElementById('inputimage');
+   const width = Number(image.width);
+   const height = Number(image.height);
+   console.log(width, height);
+   return {
+     leftCol: clarifaiFace.left_col * width,
+     topRow: clarifaiFace.top_row * height,
+     rightCol: width - (clarifaiFace.right_col * width),
+     bottomRow: height - (clarifaiFace.bottom_row * height)
+   }
+  }
+
+
+  displayFacebox = (box) => {
+    console.log(box);
+    this.setState({box: box})
   }
 
   onInputChange = (event) => {
@@ -43,18 +64,14 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input });
     //clarify api starts here
-    app.models.predict(
-      Clarifai.FACE_DETECT_MODEL, 
-      this.state.input)
-      .then(
-    function(response) {
-      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-    },
-    function(err) {
-      // there was an error
-    }
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL, 
+        this.state.input)
+      .then(response => this.displayFacebox(this.calculateFaceLocation(response))) 
+      .catch(err => console.log(err));
+      
     //clarify api ends here
-  );
   }
 
   render(){
